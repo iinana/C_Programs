7d0ae5651c23c22860232d461b28f54e3d8c0c3e
ina <jia3527@gmail.com>
2023-10-11 17:10:19 +0300

    CH23-1 finish

--- 23_prac_io.c
+++ 23_prac_io.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-    
+    FILE *fa = fopen("./C_Start_3/a.txt", "r");
+    FILE *fb = fopen("./C_Start_3/b.txt", "w");
+
+    if ((fa == NULL) || (fb == NULL)) 
+    {
+        printf("File Input Failed\n");
+        exit(1);
+    }
+
+    char c;
+    while ((c = fgetc(fa)) != EOF) fputc(c, fb);
+
+    fclose(fa);
+    fclose(fb);
 }
